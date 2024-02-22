39cbb7bb96012d22a033919aa1a664eca7a38d41
FloatingInt <knutolaihelgesen@gmail.com>
2024-02-22 17:20:28 +0100

    Initial commit

---
+++ script.py
@@ -0,0 +1,37 @@
+__version__ = "0.2.3"
+
+import pathlib
+import argparse
+import clipboard
+import color
+from color import paint
+
+
+class ParserArgs(argparse.Namespace):
+    end: str
+
+
+parser = argparse.ArgumentParser(
+    prog="Here",
+    usage="Copy 'here' path to clipboard"
+    )
+parser.add_argument("end",
+                    nargs="?",
+                    default=".",
+                    help="Relative path to join with current working directory path")
+parser.add_argument("-v", "--version",
+                    action="version",
+                    version=f"%(prog)s: v{__version__}")
+args = ParserArgs()
+parser.parse_args(namespace=args)
+
+absolute_path = (pathlib.Path
+                 .cwd()
+                 .joinpath(args.end)
+                 .resolve())
+
+print(paint("[Info]", color.AQUA),
+      paint("Copying to clipboard", color.PINK) + paint(":", color.WHITE),
+      paint(absolute_path, color.UNDERLINE + color.DARK_VIOLET))
+
+clipboard.copy(str(absolute_path))

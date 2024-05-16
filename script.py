152f8c4c15f56c25f1a1f308228ae0776920dcca
FloatingInt <knutolaihelgesen@gmail.com>
2024-05-16 14:02:57 +0200

    Moved parser argument block

--- script.py
+++ script.py
@@ -15,13 +15,13 @@ parser = argparse.ArgumentParser(
     prog="Here",
     usage="Copy 'here' path to clipboard"
     )
+parser.add_argument("-v", "--version",
+                    action="version",
+                    version=f"%(prog)s: v{__version__}")
 parser.add_argument("end",
                     nargs="?",
                     default=".",
                     help="Relative path to join with current working directory path")
-parser.add_argument("-v", "--version",
-                    action="version",
-                    version=f"%(prog)s: v{__version__}")
 args = ParserArgs()
 parser.parse_args(namespace=args)
 
